ee0933ea4ae327914cf942159575fa21bce369c1
inchingforward <inchingforward@gmail.com>
2015-09-07 22:19:37 -0500

    Removed junk.

--- testing/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils] [testing.core]))
-(figwheel.client/start {:build-id "build_needs_id_1524", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
