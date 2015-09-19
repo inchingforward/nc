1221a53520fbde5e79484e10f0f1685b75574ed0
inchingforward <inchingforward@gmail.com>
2015-09-19 09:12:08 -0500

    Removed cruft.

--- 04-mousewalker/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils] [mousewalker.core]))
-(figwheel.client/start {:build-id "build_needs_id_1420", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
